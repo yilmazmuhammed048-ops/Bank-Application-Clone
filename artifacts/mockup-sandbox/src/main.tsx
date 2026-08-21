0ab4b169d8c815b0ee66a392a2ad70cbbc0d6dea
yilmazmuhammed048-ops <yilmazmuhammed048@gmail.com>
2026-08-22 01:59:05 +0300

    Load shortcut reference styling

--- artifacts/mockup-sandbox/src/main.tsx
+++ artifacts/mockup-sandbox/src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import AdminApp from "./admin/AdminApp";
 import "./index.css";
 import "./home-reference.css";
+import "./shortcut-reference.css";
 
 const hostname = window.location.hostname.toLowerCase();
 const isAdminHost = hostname.startsWith("banka-yonetim-paneli");
