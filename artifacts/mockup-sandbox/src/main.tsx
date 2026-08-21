e0cc0ff4c4a4019752736e801ce82e00ac407203
yilmazmuhammed048-ops <yilmazmuhammed048@gmail.com>
2026-08-22 02:19:50 +0300

    Load matched header background

--- artifacts/mockup-sandbox/src/main.tsx
+++ artifacts/mockup-sandbox/src/main.tsx
@@ -4,6 +4,7 @@ import AdminApp from "./admin/AdminApp";
 import "./index.css";
 import "./home-reference.css";
 import "./shortcut-reference.css";
+import "./header-background-reference.css";
 
 const hostname = window.location.hostname.toLowerCase();
 const isAdminHost = hostname.startsWith("banka-yonetim-paneli");
