226e7ecfd74b1ae87536584fd09996160f2aa8c8
yilmazmuhammed048-ops <yilmazmuhammed048@gmail.com>
2026-08-22 06:34:46 +0300

    Revert PDF sharing changes

--- artifacts/mockup-sandbox/src/main.tsx
+++ artifacts/mockup-sandbox/src/main.tsx
@@ -7,7 +7,6 @@ import "./shortcut-reference.css";
 import "./header-background-reference.css";
 import "./header-static-final.css";
 import "./header-live-reference.css";
-import "./three-dot-pdf-share-v3";
 
 const hostname = window.location.hostname.toLowerCase();
 const isAdminHost = hostname.startsWith("banka-yonetim-paneli");
