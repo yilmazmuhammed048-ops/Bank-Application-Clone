b22b0765e32dda90e9f93ca231f91d59460e5c42
yilmazmuhammed048-ops <yilmazmuhammed048@gmail.com>
2026-08-23 00:05:04 +0300

    Use final current-movements PDF exporter

--- artifacts/mockup-sandbox/src/main.tsx
+++ artifacts/mockup-sandbox/src/main.tsx
@@ -8,8 +8,7 @@ import "./header-background-reference.css";
 import "./header-static-final.css";
 import "./header-live-reference.css";
 import "./transactions-typography-reference.css";
-import "./transactions-pdf-render-fix";
-import "./transactions-pdf";
+import "./transactions-pdf-reference-final";
 import "./login-safearea-reference";
 
 const hostname = window.location.hostname.toLowerCase();
