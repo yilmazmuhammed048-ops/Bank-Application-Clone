f18d990f13a173d548ab7513567d4b9a0d4305ef
yilmazmuhammed048-ops <yilmazmuhammed048@gmail.com>
2026-08-25 02:15:04 +0300

    Load receipt description line patch

--- artifacts/mockup-sandbox/src/transactions-pdf-reference-final.ts
+++ artifacts/mockup-sandbox/src/transactions-pdf-reference-final.ts
@@ -1,4 +1,5 @@
 import "./panel-authority-sync";
+import "./receipt-pdf-description-line";
 import "./transactions-pdf-runtime-fix";
 import "./transactions-pdf-footer-text-patch";
 import "./transactions-pdf-file-save";
